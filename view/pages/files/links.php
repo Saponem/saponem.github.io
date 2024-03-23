25a0af9a8e4950ef8fb157d2efcf736b693fae9c
Saponem <164479826+Saponem@users.noreply.github.com>
2024-03-23 12:25:48 +0300

    add files

---
+++ view/pages/files/links.php
@@ -0,0 +1,36 @@
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="view/pages/lib/easing/easing.min.js"></script>
+    <script src="view/pages/lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <!-- Contact Javascript File -->
+    <script src="view/pages/mail/jqBootstrapValidation.min.js"></script>
+    <script src="view/pages/mail/contact.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="view/pages/js/main.js"></script>
+
+      <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="view/pages/plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="view/pages/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="view/pages/dist/css/adminlte.min.css">
+<!-- jQuery -->
+<script src=view/pages/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="view/pages/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="view/pages/dist/js/adminlte.min.js"></script>
+</body>
+</html>
+
